b02e2f4dfcfecc8c071cc193b00fa6d027969e0f
Alonso0099 <aalvaradovargas@byupathway.edu>
2026-05-08 07:40:16 -0600

    Home page for wdd 231 first commit

---
+++ scripts/homepage.js
@@ -0,0 +1,10 @@
+// store the selected elements
+const navButton = document.querySelector('#ham-btn');
+const navBar = document.querySelector('#nav-bar');
+
+//toggle the show class off and on
+navButton,addEventListener('click', () => {
+  navButton.classList.toggle('show');
+  navBar.classList.toggle('show');
+});
+
