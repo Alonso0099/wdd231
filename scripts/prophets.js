ff60680aa35a240e7db556e7bf6171e80ad23e0f
Alonso0099 <alonsoj.alvarado@gmail.com>
2026-05-13 15:52:34 -0600

    Add prophets fetch data and responsive card layout

--- scripts/prophets.js
+++ scripts/prophets.js
@@ -4,7 +4,41 @@ const cards = document.querySelector('#cards');
 async function getProphetData() {
   const response = await fetch(url); // request
   const data = await response.json(); // parse the JSON data
-  console.table(data.prophets); // temp output test of data response 
+  //console.table(data.prophets); // temp output test of data response 
+  displayProphets(data.prophets); // output the prophets data
+
 }
 
 getProphetData();
+
+const displayProphets = (prophets) => {
+  prophets.forEach((prophet) => {
+    // Create elements to add to the div.cards element
+    let card = document.createElement('article');
+    let fullName = document.createElement('h2'); // fill in the blank
+    let birth = document.createElement('p');
+    let birthplace = document.createElement('p');
+    let portrait = document.createElement('img');
+
+    // Build the h2 content out to show the prophet's full name
+    fullName.textContent = `${prophet.name} ${prophet.lastname}`; // fill in the blank
+    birth.textContent = `Date of birth: ${prophet.birthdate}`;
+    birthplace.textContent = `Birthplace: ${prophet.birthplace}`; 
+    // Build the image portrait by setting all the relevant attributes
+    portrait.setAttribute('src', prophet.imageurl);
+    portrait.setAttribute('alt', `Portrait of ${prophet.name} ${prophet.lastname}`); // fill in the blank
+    portrait.setAttribute('loading', 'lazy');
+    portrait.setAttribute('width', '200');
+    portrait.setAttribute('height', '200');
+
+
+    // Append the section(card) with the created elements
+    card.appendChild(fullName); //fill in the blank
+    card.appendChild(birth);
+    card.appendChild(birthplace);
+    card.appendChild(portrait);
+
+    cards.appendChild(card); // add the card to the div.cards element
+
+  }); // end of arrow function and forEach loop
+}
