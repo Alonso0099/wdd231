3c3e993687a4ce2f38aad80624d5e4f681b6506a
Alonso0099 <aalvaradovargas@byupathway.edu>
2026-05-27 07:22:10 -0600

    Add course details modal for course cards

--- scripts/homepage.js
+++ scripts/homepage.js
@@ -25,20 +25,7 @@ const courses = [
         certificate: 'Web and Computer Programming',
         description: 'This course will introduce students to programming. It will introduce the building blocks of programming languages (variables, decisions, calculations, loops, array, and input/output) and use them to solve problems.',
         technology: [
-            'Python'
-        ],
-        completed: true
-    },
-    {
-        subject: 'WDD',
-        number: 130,
-        title: 'Web Fundamentals',
-        credits: 2,
-        certificate: 'Web and Computer Programming',
-        description: 'This course introduces students to the World Wide Web and to careers in web site design and development. The course is hands on with students actually participating in simple web designs and programming. It is anticipated that students who complete this course will understand the fields of web design and development and will have a good idea if they want to pursue this degree as a major.',
-        technology: [
-            'HTML',
-            'CSS'
+        'Python'
         ],
         completed: true
     },
@@ -50,11 +37,11 @@ const courses = [
         certificate: 'Web and Computer Programming',
         description: 'CSE 111 students become more organized, efficient, and powerful computer programmers by learning to research and call functions written by others; to write, call , debug, and test their own functions; and to handle errors within functions. CSE 111 students write programs with functions to solve problems in many disciplines, including business, physical science, human performance, and humanities.',
         technology: [
-            'Python'
+        'Python'
         ],
         completed: true
-    },
-    {
+        },
+        {
         subject: 'CSE',
         number: 210,
         title: 'Programming with Classes',
@@ -62,7 +49,21 @@ const courses = [
         certificate: 'Web and Computer Programming',
         description: 'This course will introduce the notion of classes and objects. It will present encapsulation at a conceptual level. It will also work with inheritance and polymorphism.',
         technology: [
-            'C#'
+        'C#'
+        ],
+        completed: true
+    },
+        
+    {
+        subject: 'WDD',
+        number: 130,
+        title: 'Web Fundamentals',
+        credits: 2,
+        certificate: 'Web and Computer Programming',
+        description: 'This course introduces students to the World Wide Web and to careers in web site design and development. The course is hands on with students actually participating in simple web designs and programming. It is anticipated that students who complete this course will understand the fields of web design and development and will have a good idea if they want to pursue this degree as a major.',
+        technology: [
+        'HTML',
+        'CSS'
         ],
         completed: true
     },
@@ -74,9 +75,9 @@ const courses = [
         certificate: 'Web and Computer Programming',
         description: 'This course builds on prior experience in Web Fundamentals and programming. Students will learn to create dynamic websites that use JavaScript to respond to events, update content, and create responsive user experiences.',
         technology: [
-            'HTML',
-            'CSS',
-            'JavaScript'
+        'HTML',
+        'CSS',
+        'JavaScript'
         ],
         completed: true
     },
@@ -88,9 +89,9 @@ const courses = [
         certificate: 'Web and Computer Programming',
         description: 'This course builds on prior experience with Dynamic Web Fundamentals and programming. Students will focus on user experience, accessibility, compliance, performance optimization, and basic API usage.',
         technology: [
-            'HTML',
-            'CSS',
-            'JavaScript'
+        'HTML',
+        'CSS',
+        'JavaScript'
         ],
         completed: false
     }
@@ -99,6 +100,7 @@ const courses = [
 // course rendering
 const coursesContainer = document.querySelector('#courses-container');
 const totalCredits = document.querySelector('#total-credits');
+const courseDetails = document.querySelector('#course-details');
 
 function displayCourses(coursesList) {
   coursesContainer.innerHTML  = "";
@@ -113,7 +115,9 @@ function displayCourses(coursesList) {
     } else{
       courseCard.textContent =`${course.subject} ${course.number}`;
     }
-    
+      courseCard.addEventListener('click', () => {
+    displayCourseDetails(course);
+    });
     coursesContainer.appendChild(courseCard);
   }); 
  //Display credits 
@@ -149,3 +153,25 @@ filterButtons.forEach((button) => {
 
 //Initial display
 displayCourses(courses);
+
+function displayCourseDetails(course) {
+  courseDetails.innerHTML = '';
+
+  courseDetails.innerHTML = `
+    <button id="closeModal">❌</button>
+    <h2>${course.subject} ${course.number}</h2>
+    <h3>${course.title}</h3>
+    <p><strong>Credits:</strong> ${course.credits}</p>
+    <p><strong>Certificate:</strong> ${course.certificate}</p>
+    <p>${course.description}</p>
+    <p><strong>Technologies:</strong> ${course.technology.join(', ')}</p>
+  `;
+
+  courseDetails.showModal();
+
+  const closeModal = document.querySelector('#closeModal');
+
+  closeModal.addEventListener('click', () => {
+    courseDetails.close();
+  });
+}
